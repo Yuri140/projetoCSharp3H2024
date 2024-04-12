c1e809909a7b408830c494d8efd782dbca2cce43
Yuri140 <yurisousa.carneiro2006@gmail.com>
2024-04-12 20:20:11 -0300

    FrmPesquisar com icone

--- ProjetoLojaABC/Properties/Resources.Designer.cs
+++ ProjetoLojaABC/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace ProjetoLojaABC.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap search {
+            get {
+                object obj = ResourceManager.GetObject("search", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap shopping_bag_22119 {
             get {
                 object obj = ResourceManager.GetObject("shopping_bag_22119", resourceCulture);
