8ca0d082c045194af9713dd1e658cb5e3c533092
Yuri140 <yurisousa.carneiro2006@gmail.com>
2024-05-03 22:14:32 -0300

    Alterando banco de dados

--- dbLoja.sql
+++ dbLoja.sql
@@ -1,25 +1,76 @@
-drop database dbLoja;
- 
-create database dbLoja;
- 
-use dbLoja;
- 
-create table tbFuncionarios(
-codFunc int not null auto_increment,
-nome varchar(100),
-email varchar(100),
-cpf char(14) not null unique,
-telCel char(10),
-endereco varchar(100),
-numero char(5),
-cep char(9),
-bairro varchar(100),
-cidade varchar(100),
-estado char(100),
-primary key(codFunc)
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 04-Maio-2024 às 03:13
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
 
-insert into tbFuncionarios(nome,email,cpf,
-    telCel,endereco,numero,cep,bairro,cidade,estado)
-    values(@nome,@email,@cpf,
-    @telCel,@endereco,@numero,@cep,@bairro,@cidade,@estado);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `dbloja`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbfuncionarios`
+--
+
+CREATE TABLE `tbfuncionarios` (
+  `codFunc` int(11) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `cpf` char(14) NOT NULL,
+  `telCel` char(10) DEFAULT NULL,
+  `endereco` varchar(100) DEFAULT NULL,
+  `numero` char(5) DEFAULT NULL,
+  `cep` char(9) DEFAULT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
+  `cidade` varchar(100) DEFAULT NULL,
+  `estado` char(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `tbfuncionarios`
+--
+
+INSERT INTO `tbfuncionarios` (`codFunc`, `nome`, `email`, `cpf`, `telCel`, `endereco`, `numero`, `cep`, `bairro`, `cidade`, `estado`) VALUES
+(1, 'Pedro', 'Henrique', '212.039.131-31', '39138-2312', 'Rua Perto de Casa', '131', '93823-123', 'CIdade Dutra', 'São Paulo', 'SP'),
+(2, 'André', 'andre@gmail.com', '212.122.121-22', '13131-3113', 'AQUI', '11', '11111-111', 'Ali', 'Aqui mesmo', 'SP');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `tbfuncionarios`
+--
+ALTER TABLE `tbfuncionarios`
+  ADD PRIMARY KEY (`codFunc`),
+  ADD UNIQUE KEY `cpf` (`cpf`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tbfuncionarios`
+--
+ALTER TABLE `tbfuncionarios`
+  MODIFY `codFunc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
